690bf07c578e7fcfa2a93a6f1f4cd8ce68da8b31
Daniel Atanasov <dakh@abv.bg>
2022-02-07 12:39:49 +0200

    Rot13-Cipher

---
+++ 03.Rot13-Cipher/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace _03.Rot13_Cipher
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // TODO: Cipher the given string by ROT13 rules
+
+            string message = "Test"; // EXPECTED : Grfg
+
+            var charArr = message.ToCharArray();
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                var charNum = (int)charArr[i];
+                if (charNum >= 'a' & charNum <= 'z')
+                {
+                    if (charNum > 'm') charNum -= 13;
+                    else charNum += 13;
+                }
+                else if (charNum >= 'A' & charNum <= 'Z')
+                {
+                    if (charNum > 'M') charNum -= 13;
+                    else charNum += 13;
+                }
+                charArr[i] = (char)charNum;
+            }
+
+            
+            Console.WriteLine(new string(charArr));
+            
+        }
+    }
+}
