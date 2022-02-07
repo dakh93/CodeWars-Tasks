c3e9ec20b3171fcb9e1be87db019e652610419c9
Daniel Atanasov <dakh@abv.bg>
2022-02-07 11:55:11 +0200

    Practicing

---
+++ PaginationHelper/PagnationHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.PaginationHelper
+{
+    public class PagnationHelper<T>
+    {
+        // TODO: Complete this class
+
+        /// <summary>
+        /// Constructor, takes in a list of items and the number of items that fit within a single page
+        /// </summary>
+        /// <param name="collection">A list of items</param>
+        /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+        public PagnationHelper(List<T> collection, int itemsPerPage)
+        {
+            this.collection = collection;
+            this.itemsPerPage = itemsPerPage;
+        }
+
+
+
+
+        private IList<T> collection = new List<T>();
+        private int itemsPerPage;
+
+        /// The number of items within the collection
+        /// </summary>
+        public int ItemCount
+        {
+            get
+            {
+                return this.collection.Count();
+            }
+        }
+
+        /// <summary>
+        /// The number of pages
+        /// </summary>
+        public int PageCount 
+        {
+            get
+            {
+                if (this.ItemCount % this.itemsPerPage == 0)
+                {
+                    return this.ItemCount / this.itemsPerPage;
+                }
+                else
+                {
+                    return (int)Math.Floor((double)(this.ItemCount + (this.itemsPerPage - 1)) / this.itemsPerPage);
+                }
+               
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of items in the page at the given page index 
+        /// </summary>
+        /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+        /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+        public int PageItemCount(int pageIndex)
+        {
+            // a, b, c, d, e, f, y, u, o
+            //pages = 2
+            // items = 6
+
+            //itemsPerPage = 4
+            var resultOfItemsOfPage = -1;
+
+            if (pageIndex >= this.PageCount || pageIndex < 0)
+            {
+                return resultOfItemsOfPage;
+            }
+            else if (pageIndex == 0 && this.PageCount > 1 || pageIndex < this.PageCount - 1)
+            {
+                return this.itemsPerPage;
+            }
+            else if (pageIndex == this.PageCount - 1)
+            {
+                resultOfItemsOfPage = this.ItemCount - (this.PageCount - 1) * this.itemsPerPage;
+            }
+
+            return resultOfItemsOfPage;
+
+
+        }
+
+        /// <summary>
+        /// Returns the page index of the page containing the item at the given item index.
+        /// </summary>
+        /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+        /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+        public int PageIndex(int itemIndex)
+        {
+            var pageNum = -1;
+            if (itemIndex >= this.collection.Count || itemIndex < 0)
+            {
+                return pageNum;
+            }
+            else
+            {
+                pageNum = (int)Math.Floor((decimal)itemIndex / this.itemsPerPage);
+            }
+
+            return pageNum;
+        }
+    }
+}
+
---
+++ PaginationHelper/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.PaginationHelper
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            
+
+            var helper = new PagnationHelper<int>(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 }, 10);
+
+
+           
+
+            Console.WriteLine(helper.PageCount); //should == 2
+
+            Console.WriteLine(helper.ItemCount); //should == 6
+
+            Console.WriteLine(helper.PageItemCount(0)); //should == 4
+            Console.WriteLine(helper.PageItemCount(1)); // last page - should == 2
+            Console.WriteLine(helper.PageItemCount(2)); // should == -1 since the page is invalid
+
+            // pageIndex takes an item index and returns the page that it belongs on
+            Console.WriteLine(helper.PageIndex(5)); //should == 1 (zero based index)
+            Console.WriteLine(helper.PageIndex(2));// should == 0
+             Console.WriteLine(helper.PageIndex(20));//should == -1
+             Console.WriteLine(helper.PageIndex(-10)); //should == -1
+        }
+    }
+}
